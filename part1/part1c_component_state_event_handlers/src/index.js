6a073528da1d0af8d15164169b9fba4ad34ea41e
Lukasijus <lukasrimkuslg@Gmail.com>
2019-09-01 18:37:10 +0300

    part1c_done

--- part1/part1c_component_state_event_handlers/src/index.js
+++ part1/part1c_component_state_event_handlers/src/index.js
@@ -1,30 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+//destructuring 
+/*
+const Hello = ({ name, age }) => {
+    //helper function
+    const bornYear = () => new Date().getFullYear() - age
     return (
         <div>
             <p>
-                Hello {props.name}, you are {props.age} years old
+                Hello {name}, you are {age} years old
         </p>
+            <p>So you were probably born in {bornYear()}</p>
         </div>
     )
 }
+*/
 
-const App = () => {
-    const name = 'Peter'
-    const age = 10
+const Display = ({ counter }) => {
+    console.log(counter)
+    return (
+        <div>{counter}</div>
+    )
+}
+const Button = ({ onClick, text }) => {
+    return (
+        <button onClick={onClick}>
+            {text}
+        </button>
+    )
+}
+
+
+const App = (props) => {
+
+    const [counter, setCounter] = useState(0)
+    const setToValue = (value) => setCounter(value)
 
     return (
         <div>
-            <h1>Greetings</h1>
-            <Hello name="Maya" age={26 + 10} />
-            <Hello name={name} age={age} />
+            <Display counter={counter}></Display>
+            <Button
+                onClick={() => {
+                    setToValue(counter + 1)
+                }}
+                text='plus'
+            />
+            <Button
+                onClick={() => setToValue(counter - 1)}
+                text='minus'></Button>
+            <Button
+                onClick={() => setToValue(0)}
+                text='zero'></Button>
         </div>
+
+
     )
 }
+/*
+let counter = 1
 
+const refresh = () => {
+    ReactDOM.render(<App counter={counter}></App>, document.getElementById('root'))
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//re-render components using repeated calls to the ReactDom.render method
+setInterval(() => {
+    refresh()
+    counter += 1
+}, 1000);
+*/
+ReactDOM.render(<App></App>, document.getElementById('root'))
 
 
