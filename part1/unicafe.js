07a2eded5631cab2354749e2f0bb80e90b501134
Rimkus Lukas <lukas.rimkus@procemex.com>
2019-09-07 19:30:34 +0300

    unicafe done

--- part1/unicafe.js
+++ part1/unicafe.js
@@ -1,13 +1,28 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import { ReactComponent } from '*.svg';
 
-const App = (props) => {
+
+const Button = ({onClick, text}) => {
+    <button onClick = {onClick}>{text}</button>
+}
+
+
+const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
-    const handleClicks = (props) => {
-        props
+    const handleGoodClicks = () => {
+        setGood(good + 1)
+    }
+
+    const handleNeutralClicks = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBadClicks = () => {
+        setBad(bad + 1)
     }
 
     const Button = (props) =>{
@@ -20,12 +35,20 @@ const App = (props) => {
 
     return (
         <div>
+            <h1>give feedback</h1>
             <div>
-                <h1>give feedback</h1>
+                <Button onClick={handleGoodClicks} text = 'good'></Button>
+                <Button onClick={handleNeutralClicks} text = 'neutral'></Button>
+                <Button onClick={handleBadClicks} text = 'bad'></Button>
             </div>
+            <h1>statistics</h1>
             <div>
-                <Button handleClicks={ () => setGood}
+                <p>good: {godd}</p>
+                <p>neutral: {neutral}</p>
+                <p>bad: {bad}</p>
             </div>
         </div>
     )
 }
+
+ReactDOM.render(<App></App>, document.getElementById('root'))
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,24 +9,90 @@ const Button = ({onClick, text}) => {
     
 }
 
+const Statistics = (props) => {
+    if (props.check === 0 ) return (<h2>no feedback given</h2>)
+    return (
+        
+            <table>
+                <tbody>
+                    <tr>
+                        <td>{props.text1}</td>
+                        <td>{props.func1}</td> 
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>{props.text2}</td>
+                        <td>{props.func2}</td> 
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>{props.text3}</td>
+                        <td>{props.func3}</td> 
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>{props.text4}</td>
+                        <td>{props.func4}</td> 
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>{props.text5}</td>
+                        <td>{props.func5}</td> 
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>{props.text6}</td>
+                        <td>{props.func6}</td> 
+                    </tr>
+                </tbody>
+                                
+            </table>
+        
+    )
+}
+
 const App = (props) => {
     //define variables and states
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
+    const [average, setAverage] = useState(0)
+
     //handleGood
 const handleGoodClicks = () => {
     setGood(good + 1)
+    setAll(all + 1)
+    setAverage(average + 1)
+    
 }
 
     //handleNeutral
 const handleNeutralClicks = () => {
         setNeutral(neutral + 1)
+        setAll(all + 1)
     }
 
     //handleBad
 const handleBadClicks = () => {
     setBad(bad + 1)
+    setAll(all + 1)
+    setAverage(average - 1)
+}
+
+const calcAvg = () => {
+    if (isNaN(average/all)) return 0
+    return average/all
+}
+
+const calcPos = () => {
+    if (isNaN(good/all)) return 0 + ' %' 
+    return good/all * 100 + ' %'
 }
 
     //return JSX -> HTML
@@ -40,9 +106,13 @@ const handleBadClicks = () => {
             </div>
             <h1>statistics</h1>
             <div>
-                <p>good: {good}</p>
-                <p>neutral: {neutral}</p>
-                <p>bad: {bad}</p>
+                <Statistics   check = {all} 
+                            text1 = 'good' func1 = {good}
+                            text2 = 'neutral' func2 = {neutral}
+                            text3 = 'bad' func3 = {bad}
+                            text4 = 'all' func4 = {all}
+                            text5 = 'average' func5 = {calcAvg()}
+                            text6 = 'positive' func6 = {calcPos()}></Statistics>
             </div>
         </div>
     )
